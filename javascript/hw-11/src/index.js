32d6a1bfbaaf857f17830413b15cc22eeac05574
matuychshenko.fndrej <matvej03111984@gmail.com>
2020-10-12 22:57:25 +0300

    fix import

--- javascript/hw-11/src/index.js
+++ javascript/hw-11/src/index.js
@@ -1,8 +1,8 @@
 import './styles.css';
-// import './index1.js';
-// import './index2.1.js';
-// import './index2.2.js';
-// import './index2.3.js';
+import './index1.js';
+import './index2.1.js';
+import './index2.2.js';
+import './index2.3.js';
 import './index3.js';
 
 
